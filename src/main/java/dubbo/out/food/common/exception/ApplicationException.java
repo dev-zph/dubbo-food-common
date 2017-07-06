464b223f2243d7b5204e98c44bbf70c026f9101e
zhangph <15858331351@163.com>
2017-07-06 12:38:02 +0800

    遗弃遗留的代码

--- src/main/java/dubbo/out/food/common/exception/ApplicationException.java
+++ src/main/java/dubbo/out/food/common/exception/ApplicationException.java
@@ -1,17 +1,17 @@
-package dubbo.out.food.exception;
+package dubbo.out.food.common.exception;
 
 public class ApplicationException extends RuntimeException {
 	private static final long serialVersionUID = -1936158479822916234L;
 
 	/**
-     * 构�?�一个空的异�?.
+     * 构�?�一个空的异�?.
      */
     public ApplicationException() {
         super();
     }
 
     /**
-     * 构�?�一个异�?, 指明异常的详细信�?.
+     * 构�?�一个异�?, 指明异常的详细信�?.
      *
      * @param message 详细信息
      */
@@ -20,19 +20,19 @@ public class ApplicationException extends RuntimeException {
     }
 
     /**
-     * 构�?�一个异�?, 指明引起这个异常的起�?.
+     * 构�?�一个异�?, 指明引起这个异常的起�?.
      *
-     * @param cause 异常的起�?
+     * @param cause 异常的起�?
      */
     public ApplicationException(Throwable cause) {
         super(cause);
     }
 
     /**
-     * 构�?�一个异�?, 指明引起这个异常的起�?.
+     * 构�?�一个异�?, 指明引起这个异常的起�?.
      *
      * @param message 详细信息
-     * @param cause 异常的起�?
+     * @param cause 异常的起�?
      */
     public ApplicationException(String message, Throwable cause) {
         super(message, cause);
